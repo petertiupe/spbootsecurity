775cf5dad75511834995c686e7e5868ee4a4e104
petertiupe <peter@tiupe.de>
2022-05-20 12:54:39 +0200

    first running version without any logic copied from spring.initialzr after creation

---
+++ src/main/kotlin/de/tiupe/spbootsecurity/SpbootsecurityApplication.kt
@@ -0,0 +1,11 @@
+package de.tiupe.spbootsecurity
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpbootsecurityApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpbootsecurityApplication>(*args)
+}
---
+++ src/test/kotlin/de/tiupe/spbootsecurity/SpbootsecurityApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.tiupe.spbootsecurity
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpbootsecurityApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
