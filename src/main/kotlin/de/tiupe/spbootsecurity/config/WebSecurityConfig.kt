03d70822bd81e8a8dce17e5078c43f1644f80011
petertiupe <peter@tiupe.de>
2022-05-22 13:30:27 +0200

    Password-Encoder and AuthenticationManager-Builder added to change spring-boot-default-behaviour

--- src/main/kotlin/de/tiupe/spbootsecurity/config/WebSecurityConfig.kt
+++ src/main/kotlin/de/tiupe/spbootsecurity/config/WebSecurityConfig.kt
@@ -2,11 +2,14 @@ package de.tiupe.spbootsecurity.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.password.NoOpPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
 
 
@@ -27,6 +30,46 @@ class WebSecurityConfig : WebSecurityConfigurerAdapter() {
             .logout()
             .permitAll()
     }
+
+
+    /*
+
+    * Der AuthenticationManagerBuilder dient dazu, das Authentication Verfahren zu ändern. Spring bietet die folgende
+    * Methode an, um dem AuthenticationManagerBuilder mitzuteilen, welcher AuthenticationManager für SpringSecurity
+    * verwendet werden soll.
+    *
+    * Der Authentication Manager benötigt immer einen Passwort-Encoder, ansonsten wirft Spring einen Fehler
+    *
+    * IllegalArgumentException: There is no PasswordEncoder mapped....
+    *
+    * Da man sich hier in einer Configuration-Klasse befindet, kann man die Bean gleich hier definieren.
+    *
+    * Durch das Setzen des AuthenticationManagers funktioniert der UseDetailService so nicht mehr.
+    *
+    * Die Funktion lasse ich dennoch zur Dokumentation und zum Testen auch in diesem Branch stehen. Beide Codestellen
+    * gehen gegen den InMemorySpeicher und haben daher ein Konkurenz-Problem.
+    *
+    * */
+    override fun configure(auth: AuthenticationManagerBuilder?) {
+        auth!!.inMemoryAuthentication()
+                .withUser("Peter")
+                .password("peterwars")
+                .roles("USER")
+            // mit der and()-Funktion kann man beliebig weitere User hinzufügen
+            .and()
+                .withUser("Tina")
+                .password("tinawars")
+                .roles("USER")
+    }
+
+    /*
+    * Siehe Kommentar zu
+    *
+    *   @see override fun configure(auth: AuthenticationManagerBuilder?)
+    *
+    * */
+    @Bean
+    fun getPasswordEncoder(): PasswordEncoder = NoOpPasswordEncoder.getInstance()
 
     @Bean
     public override fun userDetailsService(): UserDetailsService {
